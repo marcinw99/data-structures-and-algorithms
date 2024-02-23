0fc9ee74e3af05f2e59d895bba31ac5f85ff99c3
Marcin Wąsik <marcinmw99@gmail.com>
2024-02-23 11:56:51 +0100

    feat: onlinestockspan best

--- StacksAndQueues/OnlineStockSpan/OnlineStockSpan.py
+++ StacksAndQueues/OnlineStockSpan/OnlineStockSpan.py
@@ -39,10 +39,14 @@ class StockSpannerStackWithQueuePosition:
 
 class StockSpannerMonoStack:
     def __init__(self):
-        self.stack = deque()
+        self.stack = []
 
     def next(self, price: int) -> int:
-        return 0
+        answer = 1
+        while self.stack and self.stack[-1][0] <= price:
+            answer += self.stack.pop()[1]
+        self.stack.append([price, answer])
+        return answer
 
 
 obj = StockSpannerMonoStack()
