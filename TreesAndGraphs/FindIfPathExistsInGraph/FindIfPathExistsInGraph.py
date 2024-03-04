4262fc8aaa57ebb8f732452b80b69b0d6f859b59
Marcin Wąsik <marcinmw99@gmail.com>
2024-03-04 18:06:13 +0100

    feat: findifpathexistsingraph final

--- TreesAndGraphs/FindIfPathExistsInGraph/FindIfPathExistsInGraph.py
+++ TreesAndGraphs/FindIfPathExistsInGraph/FindIfPathExistsInGraph.py
@@ -15,43 +15,82 @@ class Solution:
 
         visited = set()
         visited.add(source)
-        found = False
 
         def dfs(start: int):
-            nonlocal found
             for neighbour in graph[start]:
-                # if found:
-                #     return True
                 if neighbour not in visited:
                     visited.add(neighbour)
                     if neighbour == destination:
-                        found = True
-                    else:
-                        dfs(neighbour)
+                        return True
+                    elif dfs(neighbour):
+                        return True
 
-        dfs(source)
+        return dfs(source) or False
 
-        return found
+    def validPathIterative(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        if source == destination:
+            return True
 
-    def validPathBetter(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        return True
+        graph = defaultdict(list)
+
+        for x, y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+
+        visited = set()
+        visited.add(source)
+
+        stack = [source]
+
+        while len(stack) > 0:
+            node = stack.pop()
+            for neighbour in graph[node]:
+                if neighbour not in visited:
+                    visited.add(neighbour)
+                    if neighbour == destination:
+                        return True
+                    stack.append(neighbour)
+
+        return False
+
+    def validPathDFSBetter(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = defaultdict(list)
+
+        for x, y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+
+        visited = set()
+
+        def dfs(node: int):
+            if node == destination:
+                return True
+            if node not in visited:
+                visited.add(node)
+                for neighbour in graph[node]:
+                    if dfs(neighbour):
+                        return True
+            return False
+
+        return dfs(source)
 
 
 def test1():
-    print(Solution().validPathBetter(3, [[0, 1], [1, 2], [2, 0]], 0, 2))  # True
+    print(Solution().validPath(3, [[0, 1], [1, 2], [2, 0]], 0, 2))  # True
 
 
 def test2():
-    print(Solution().validPathBetter(5, [[0, 1], [0, 2], [3, 5], [5, 4], [4, 3]], 0, 5))  # False
+    print(Solution().validPath(5, [[0, 1], [0, 2], [3, 5], [5, 4], [4, 3]], 0, 5))  # False
 
 
 def test3():
     print(
-        Solution().validPathBetter(10, [[4, 3], [1, 4], [4, 8], [1, 7], [6, 4], [4, 2], [7, 4], [4, 0], [0, 9], [5, 4]],
-                                   5,
-                                   9))  # True
+        Solution().validPath(10,
+                             [[4, 3], [1, 4], [4, 8], [1, 7], [6, 4], [4, 2], [7, 4], [4, 0], [0, 9], [5, 4]],
+                             5,
+                             9))  # True
 
 
-# test1()
-# test2()
+test1()
+test2()
 test3()
