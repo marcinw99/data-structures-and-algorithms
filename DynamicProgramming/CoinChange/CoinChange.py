48803fd66efdc5cb33ceb23f956dae4146445514
Marcin Wąsik <marcinmw99@gmail.com>
2024-03-29 12:36:58 +0100

    feat: base CoinChange

--- DynamicProgramming/CoinChange/CoinChange.py
+++ DynamicProgramming/CoinChange/CoinChange.py
@@ -3,15 +3,33 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        sorted_coins = sorted(coins, reverse=True)
 
-        def dp():
-            return 0
+        def dp(remaining_amount):
+            if remaining_amount in cache:
+                return cache[remaining_amount]
 
-        return dp()
+            if remaining_amount == 0:
+                return 0
+
+            fewest = float("+inf")
+            for coin in sorted_coins:
+                diff = remaining_amount - coin
+                if diff >= 0:
+                    count = dp(diff) + 1
+                    fewest = min(count, fewest)
+
+            cache[remaining_amount] = fewest
+            return cache[remaining_amount]
+
+        cache = {}
+        result = dp(amount)
+        return result if result != float("+inf") else -1
 
 
 def test1():
     print(Solution().coinChange([1, 2, 5], 11))  # 3
+    print(Solution().coinChange([3, 5], 11))  # 3
 
 
 def test2():
